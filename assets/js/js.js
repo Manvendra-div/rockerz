9b5dc94a47d7cb1de9b850cf49e893ae34e22bad
Manvendra Singh <67854646+Manvendra-div@users.noreply.github.com>
2022-06-25 14:31:28 +0530

    Add files via upload

---
+++ assets/js/js.js
@@ -0,0 +1,27 @@
+let userAgent = navigator.appVersion;
+let osDetails = {
+    name: 'your device is not available',
+    icon: 'fa-frown-o',
+    link: '#'
+};
+if (userAgent.includes('Windows')) {
+    osDetails.name = 'Windows';
+    osDetails.icon = 'fa-windows';
+    osDetails.link = "https://gitlab.com/Manvendra-div/rockerz-release/-/blob/main/rockerz_setup.exe";
+}
+
+if (userAgent.includes('Linux')) {
+    osDetails.name = 'Linux';
+    osDetails.icon = 'fa-linux';
+    osDetails.link = "sorry.html";
+}
+updateOsDownloadButton(osDetails);
+afterClick(osDetails);
+
+function updateOsDownloadButton(osDetails) {
+  let iconHtml = '<i class="fa fa-star ' + osDetails.icon + '"></i>'
+  document.querySelector('.btn-primary').innerHTML = 'Download for ' + osDetails.name + iconHtml
+}
+
+function afterClick(osDetails) {document.getElementById("downloadbtn").href=osDetails.link;
+}
